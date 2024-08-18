e61adcb0294cd740c44267e73dd3f030572877c6
dimpho-01 <dimpho.matlou@gmail.com>
2024-08-18 19:44:44 +0200

    Add user authentication

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
 
 # Create your views here.
 def list_books(request):
