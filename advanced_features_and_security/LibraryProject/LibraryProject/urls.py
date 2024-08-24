41fbc2c0afef7fac502f0ccd8f1fa889e6966331
dimpho-01 <dimpho.matlou@gmail.com>
2024-08-24 07:01:16 +0200

    Manage permissions and groups

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.book_list, name='book_list'),
+    path('books/create/', views.create_book, name='create_book'),
+    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,16 +2,31 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views import View
+from .models import Book
 
 # Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, 'bookshelf/book_list.html', context)
+
+
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
-    # Logic to edit a book
+    # The logic to edit a book goes here
     pass
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def view_books(request):
-    # Logic to view books
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # The logic to create a new book goes here
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    # The logic to delete a book goes here
     pass
 
 class EditBookView(PermissionRequiredMixin, View):
