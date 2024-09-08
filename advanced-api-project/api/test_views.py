bd022752c40f10c811b283a9fa198bdf15d170d1
dimpho-01 <dimpho.matlou@gmail.com>
2024-09-08 21:22:40 +0200

    Modify test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,22 +3,18 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from api.models import Author, Book
-from api.serializers import BookSerializer
+from rest_framework import status
+from django.urls import reverse
 
 class BookAPITestCase(APITestCase):
     
     def setUp(self):
-        # Set up initial data and test user.
+        # Set up initial data and a test user.
         self.user = User.objects.create_user(username='testuser', password='password')
         self.author = Author.objects.create(name='Author Name')
-        self.book = Book.objects.create(
-            title="Test Book",
-            publication_year=2020,
-            author=self.author
-        )
-
+        
     def test_create_book(self):
-        self.client.login(username='testuser', password='password')
+        self.client.force_authenticate(user=self.user)  # Ensure the user is logged in for this test
         url = reverse('book-create')
         data = {
             'title': "Another Test Book",
@@ -26,6 +22,9 @@ class BookAPITestCase(APITestCase):
             'author': self.author.id
         }
         response = self.client.post(url, data, format='json')
+        
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Book.objects.count(), 2)
-        self.assertEqual(Book.objects.latest('id').title, data['title'])
+        self.assertEqual(response.data['title'], data['title'])
+        self.assertEqual(response.data['author'], data['author'])
+        self.assertEqual(response.data['publication_year'], data['publication_year'])
+        self.assertEqual(Book.objects.count(), 1)
