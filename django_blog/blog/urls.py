5f09e32aab38a10048dcf71ef83dfaecec65e48e
dimpho-01 <dimpho.matlou@gmail.com>
2024-09-14 20:15:28 +0200

    Add user authentication system

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # Redirect to home or next page
+            return redirect('home')
+        # If the form is not valid, the same page with form errors is rendered
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        # Create a form instance with the submitted data
+        form = ProfileForm(request.POST, instance=request.user)
+        
+        if form.is_valid():
+            # Save the changes to the user object
+            form.save()
+            messages.success(request, f'Your profile has been updated.')
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/profile.html', context)
